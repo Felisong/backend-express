24d33dfc9019dc6a9bccb6c14ab300d5033a21ac
carolina <Carolinahs100@gmail.com>
2025-03-17 20:12:19 -0600

    initial commit for backend.

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/routes";
+import mongoose from "mongoose";
+
+dotenv.config(); // Load environment variables
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+const MONGO_URI = process.env.MONGO_URI || "";
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+
+app.use("/api", userRoutes);
+
+// Test route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from TypeScript + Express!");
+});
+
+// Set up server port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} : http://localhost:5000`);
+});
