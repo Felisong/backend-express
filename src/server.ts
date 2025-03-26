6d29d5b3a1229a88ff893fdd37497ddcd52db34b
carolina <Carolinahs100@gmail.com>
2025-03-25 23:16:38 -0600

    please.

--- src/server.ts
+++ src/server.ts
@@ -31,10 +31,7 @@ app.use(
 
 const MONGO_URI = process.env.MONGO_URI || "";
 mongoose
-  .connect(MONGO_URI, {
-    serverSelectionTimeoutMS: 5000, // 5 second timeout
-    socketTimeoutMS: 45000,
-  })
+  .connect(MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => {
     console.error("❌ Fatal MongoDB Error:", err);
