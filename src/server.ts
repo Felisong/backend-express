d3115a8d9371d37f33f4db53cccd633cb133379e
carolina <Carolinahs100@gmail.com>
2025-03-25 21:59:06 -0600

    test.

--- src/server.ts
+++ src/server.ts
@@ -9,11 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://portfolio-site-front.vercel.app",
-  })
-);
+app.use(cors());
 
 const MONGO_URI = process.env.MONGO_URI || "";
 mongoose
