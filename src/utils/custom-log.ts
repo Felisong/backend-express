66961fba34e31cf1adcec0533cfa5ce0e96b3166
carolina <Carolinahs100@gmail.com>
2025-07-30 00:30:28 -0600

    added a new path to receive messages from people.

--- src/utils/custom-log.ts
+++ src/utils/custom-log.ts
@@ -1,5 +1,5 @@
 export const log = {
-  info: (args: string) => console.log("%c[INFO]", "color: green;", args),
-  warn: (args: string) => console.warn("%c[WARN]", "color: orange;", args),
-  error: (args: string) => console.error("%c[ERROR]", "color: red;", args),
+  info: (args: any[]) => console.log("%c[INFO]", "color: green;", args),
+  warn: (args: any[]) => console.warn("%c[WARN]", "color: orange;", args),
+  error: (args: any[]) => console.error("%c[ERROR]", "color: red;", args),
 };
